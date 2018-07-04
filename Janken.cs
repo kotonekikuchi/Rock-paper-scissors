a71297f9cb03caea90b41cd039c41b8893bc58b7
kotonekikuchi <kotone.kikuchi@diva.co.jp>
2018-07-04 17:42:10 +0900

    複雑性を下げた

---
+++ Janken.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Janken.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ConsoleApp1
+{
+    using System;
+
+    public static class Janken
+    {
+        public static int UserNum = 0;
+        public static int CpuNum = 0;
+        public static int[] Hands = null;
+        public static bool[] Results = null;
+        public static int rock = 1;
+        public static int Paper = 2;
+        public static int scissors = 3;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+﻿// <copyright file="Janken.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
@@ -10,185 +10,27 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            int personCheck = 0; // 人数入力の数値チェック
-            int usernum = 0; // ユーザの人数
-            int cpunum = 0; // CPUの人数
-            int[] hands = null; // 人数分のじゃんけんの手を保持する配列
-            int[] results = null; // プレーヤーごとのじゃんけんの結果を保持する配列
-            int rock = 1; // グー
-            int paper = 2; // チョキ
-            int scissors = 3; // パー
-
-            while (personCheck == 0)
+            PlayerNum(); // プレイヤーの人数を設定
+            bool retryFlg = true;
+            while (retryFlg)
             {
-                // じゃんけんの参加人数を決める。
-                Console.WriteLine("ユーザの人数を入力してください。");
-                var userr = Console.ReadLine();
-                Console.WriteLine("対戦したいCPUの数を入力してください。");
-                var cpur = Console.ReadLine();
-                if (int.TryParse(userr, out int un) && int.TryParse(cpur, out int cn))
+                bool drowFlg = true; // あいこ判定
+                while (drowFlg)
                 {
-                    usernum = un;
-                    cpunum = cn;
-                    personCheck = 1;
-                    hands = new int[un + cn];
-                    results = new int[un + cn];
-                }
-                else
-                {
-                    personCheck = 0;
-                    Console.WriteLine("数値を入力してください。");
-                }
-            }
+                    // ユーザの手を決める。
+                    SelectUserHand();
 
-            int battlefinish = 0; // あいこ判定
-            while (battlefinish == 0)
-            {
-                // ユーザに手を決めてもらう。
-                for (int i = 0; i < usernum; i++)
-                {
-                    int check = 0;
-                    while (check == 0)
-                    {
-                        Console.WriteLine("ユーザ" + (i + 1) + "の手を入力してください。1:グー 2:チョキ 3:パー");
-                        var uhand = Console.ReadLine();
-                        check = CheckJankenNum(uhand);
-                        if (check == 0)
-                        {
-                            Console.WriteLine("1~3の数値を入力してください。");
-                        }
-                        else if (check == 1)
-                        {
-                            hands[i] = int.Parse(uhand);
-                        }
-                    }
+                    // CPUの手を決める。
+                    SelectCpuHand();
+
+                    // じゃんけんの結果判定
+                    drowFlg = CheckJankenResult();
                 }
 
-                // CPUの手を決める。
-                for (int i = 0; i < cpunum; i++)
-                {
-                    hands[usernum + i] = new System.Random().Next(1, 4);
-                }
-
-                int rockNum = 0; // グーを出している人数
-                int paperNum = 0; // パーを出している人数
-                int scissorsNum = 0; // チョキを出している人数
-
-                // じゃんけんの処理を行う。
-                for (int i = 0; i < hands.Length; i++)
-                {
-                    switch (hands[i])
-                    {
-                        case 1:
-                            rockNum++;
-                            break;
-                        case 2:
-                            scissorsNum++;
-                            break;
-                        case 3:
-                            paperNum++;
-                            break;
-                    }
-                }
-
-                // あいこの場合
-                if ((rockNum > 0 && paperNum > 0 && scissorsNum > 0) || rockNum == (usernum + cpunum) || paperNum == (usernum + cpunum) || (scissorsNum == usernum + cpunum))
-                {
-                    Console.WriteLine("あいこです。");
-                    battlefinish = 0;
-                }
-                else
-                {
-                    // チョキが勝ちの場合
-                    if (rockNum == 0)
-                    {
-                        for (int i = 0; i < hands.Length; i++)
-                        {
-                            if (hands[i] == rock)
-                            {
-                                results[i] = 1;
-                            }
-                            else if (hands[i] == paper)
-                            {
-                                results[i] = 0;
-                            }
-                        }
-                    } // パーが勝ちの場合
-                    else if (scissorsNum == 0)
-                    {
-                        for (int i = 0; i < hands.Length; i++)
-                        {
-                            if (hands[i] == paper)
-                            {
-                                results[i] = 1;
-                            }
-                            else if (hands[i] == scissors)
-                            {
-                                results[i] = 0;
-                            }
-                        }
-                    }// グーが勝ちの場合
-                    else if (paperNum == 0)
-                    {
-                        for (int i = 0; i < hands.Length; i++)
-                        {
-                            if (hands[i] == rock)
-                            {
-                                results[i] = 1;
-                            }
-                            else if (hands[i] == scissors)
-                            {
-                                results[i] = 0;
-                            }
-                        }
-                    }
-
-                    // ユーザごとの勝ち負けの判断をし、結果を出力する。
-                    for (int i = 0; i < usernum; i++)
-                    {
-                        if (results[i] == 1)
-                        {
-                            Console.WriteLine("ユーザ" + (i + 1) + "は勝ちです。");
-                        }
-                        else if (results[i] == 0)
-                        {
-                            Console.WriteLine("ユーザ" + (i + 1) + "は負けです。");
-                        }
-                    }
-                    int retryflg = 0;
-                    while (retryflg == 0)
-                    {
-                        Console.WriteLine("リトライしますか？ 1:はい 2:いいえ");
-                        var retry = Console.ReadLine();
-                        if (int.TryParse(retry, out int re))
-                        {
-                            if (re <= 0 || re >= 3)
-                            {
-                                Console.WriteLine("1,2いずれかの数値を入力してください。");
-                            }
-                            else
-                            {
-                                retryflg = 1;
-                                if (re == 1)
-                                {
-                                    battlefinish = 0;
-                                }
-                                else if (re == 2)
-                                {
-                                    battlefinish = 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("1,2いずれかの数値を入力してください。");
-                        }
-                    }
-
-                }
+                ResultOutput(); // 結果の出力を行う。
+                retryFlg = CheckRetry(); // リトライするかどうか
             }
         }
-
 
         // じゃんけんの手1~3を出している場合は1、それ以外を出している場合は0を返す。
         private static int CheckJankenNum(string input)
@@ -206,6 +48,208 @@ namespace ConsoleApp1
             }
 
             return 0;
+        }
+
+        // プレイヤーの人数を設定する。
+        private static void PlayerNum()
+        {
+            bool personCheckFlg = false;
+            while (!personCheckFlg)
+            {
+                // じゃんけんの参加人数を決める。
+                Console.WriteLine("ユーザの人数を入力してください。");
+                var userr = Console.ReadLine();
+                Console.WriteLine("対戦したいCPUの数を入力してください。");
+                var cpur = Console.ReadLine();
+                if (int.TryParse(userr, out int un) && int.TryParse(cpur, out int cn))
+                {
+                    if (un != 0 && cn != 0)
+                    {
+                        Janken.UserNum = un;
+                        Janken.CpuNum = cn;
+                        personCheckFlg = true;
+                        Janken.Hands = new int[un + cn];
+                        Janken.Results = new bool[un + cn];
+                    }
+                    else
+                    {
+                        Console.WriteLine("1以上の数字を入力してください。");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("数値を入力してください。");
+                }
+            }
+        }
+
+        // ユーザに手を決めてもらう。
+        private static void SelectUserHand()
+        {
+            for (int i = 0; i < Janken.UserNum; i++)
+            {
+                int check = 0;
+                while (check == 0)
+                {
+                    Console.WriteLine("ユーザ" + (i + 1) + "の手を入力してください。1:グー 2:チョキ 3:パー");
+                    var uhand = Console.ReadLine();
+                    check = CheckJankenNum(uhand);
+                    if (check == 0)
+                    {
+                        Console.WriteLine("1~3の数値を入力してください。");
+                    }
+                    else if (check == 1)
+                    {
+                        Janken.Hands[i] = int.Parse(uhand);
+                    }
+                }
+            }
+        }
+
+        private static void SelectCpuHand()
+        {
+            for (int i = 0; i < Janken.CpuNum; i++)
+            {
+                Janken.Hands[Janken.UserNum + i] = new System.Random().Next(1, 4);
+            }
+        }
+
+        private static bool CheckJankenResult()
+        {
+            int rockNum = 0; // グーを出している人数
+            int paperNum = 0; // パーを出している人数
+            int scissorsNum = 0; // チョキを出している人数
+            bool drowFlg = true;
+
+            // じゃんけんの処理を行う。
+            for (int i = 0; i < Janken.Hands.Length; i++)
+            {
+                switch (Janken.Hands[i])
+                {
+                    case 1:
+                        rockNum++;
+                        break;
+                    case 2:
+                        scissorsNum++;
+                        break;
+                    case 3:
+                        paperNum++;
+                        break;
+                }
+            }
+
+            drowFlg = CheckWinner(rockNum, scissorsNum, paperNum);
+            return drowFlg;
+        }
+
+        private static bool CheckWinner(int rockNum, int paperNum, int scissorsNum)
+        {
+            bool drowFlg = true;
+
+            // あいこの場合
+            if ((rockNum > 0 && paperNum > 0 && scissorsNum > 0)
+                || rockNum == (Janken.UserNum + Janken.CpuNum)
+                || paperNum == (Janken.UserNum + Janken.CpuNum)
+                || (scissorsNum == Janken.UserNum + Janken.CpuNum))
+            {
+                Console.WriteLine("あいこです。");
+                drowFlg = true;
+            }
+            else
+            {
+                drowFlg = false;
+                for (int i = 0; i < Janken.Hands.Length; i++)
+                {
+                    // チョキが勝ちの場合
+                    if (rockNum == 0)
+                    {
+                        if (Janken.Hands[i] == Janken.rock)
+                        {
+                            Janken.Results[i] = true;
+                        }
+                        else if (Janken.Hands[i] == Janken.Paper)
+                        {
+                            Janken.Results[i] = false;
+                        }
+                    }// パーが勝ちの場合
+                    else if (scissorsNum == 0)
+                    {
+                        if (Janken.Hands[i] == Janken.Paper)
+                        {
+                            Janken.Results[i] = true;
+                        }
+                        else if (Janken.Hands[i] == Janken.scissors)
+                        {
+                            Janken.Results[i] = false;
+                        }
+                    }// グーが勝ちの場合
+                    else if (paperNum == 0)
+                    {
+                        if (Janken.Hands[i] == Janken.rock)
+                        {
+                            Janken.Results[i] = true;
+                        }
+                        else if (Janken.Hands[i] == Janken.scissors)
+                        {
+                            Janken.Results[i] = false;
+                        }
+                    }
+                }
+            }
+
+            return drowFlg;
+        }
+
+        private static void ResultOutput() // 勝ち負けを出力する。
+        {
+            for (int i = 0; i < Janken.UserNum; i++)
+            {
+                if (Janken.Results[i])
+                {
+                    Console.WriteLine("ユーザ" + (i + 1) + "は勝ちです。");
+                }
+                else if (!Janken.Results[i])
+                {
+                    Console.WriteLine("ユーザ" + (i + 1) + "は負けです。");
+                }
+            }
+        }
+
+        private static bool CheckRetry() // リトライするかのチェック
+        {
+            bool retryFlg = false; // リトライするかどうか
+            bool checkRetry = false; // リトライの入力が正しく行われているかどうか
+            while (!checkRetry)
+            {
+                Console.WriteLine("リトライしますか？ 1:はい 2:いいえ");
+                var retry = Console.ReadLine();
+                if (int.TryParse(retry, out int re))
+                {
+                    if (re <= 0 || re >= 3)
+                    {
+                        Console.WriteLine("1,2いずれかの数値を入力してください。");
+                    }
+                    else
+                    {
+                        checkRetry = true;
+                        if (re == 1)
+                        {
+                            retryFlg = true;
+                        }
+                        else if (re == 2)
+                        {
+                            retryFlg = false;
+                        }
+                    }
+                }
+                else
+                {
+                    checkRetry = false;
+                    Console.WriteLine("1,2いずれかの数値を入力してください。");
+                }
+            }
+
+            return retryFlg;
         }
     }
 }
